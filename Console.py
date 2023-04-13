d9f7e9b0c03e20f42eab8e43b9c77e61d8c4e570
Harsha <khurdhulaharshavardhan@gmail.com>
2023-04-13 19:22:14 -0400

    adds Resource Console.py which acts as user interface.

---
+++ Console.py
@@ -0,0 +1,10 @@
+
+class Console():
+    """
+    This module acts as a interface between our user and our Expert System.
+    It prompts user to enter their symptoms and based on that the input is communicated to Expert System.
+    These results are then returned.
+    """
+
+    def __init__(self) -> None:
+        pass
