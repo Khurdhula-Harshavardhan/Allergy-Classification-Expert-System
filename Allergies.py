722f44e65043d444c475b83c98ed116f72b5e72b
Harsha <khurdhulaharshavardhan@gmail.com>
2023-04-13 18:21:17 -0400

    Adds resource Class Allergy that has core properties and functionality.

---
+++ Allergies.py
@@ -0,0 +1,7 @@
+
+class Allergy():
+    """
+    The class Allergy has simple on memory list of Allergies and their respective symptoms.
+    It also has a method that can predict possible allergies.
+    """
+    pass
