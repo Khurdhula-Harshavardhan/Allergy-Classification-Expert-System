5518fbb95b0959f98cf53bba320595d52e0e2db5
Harsha <khurdhulaharshavardhan@gmail.com>
2023-04-13 19:26:39 -0400

    adds run method which starts the expert system.

--- Console.py
+++ Console.py
@@ -1,3 +1,5 @@
+from Allergies import Allergy
+
 
 class Console():
     """
@@ -5,6 +7,19 @@ class Console():
     It prompts user to enter their symptoms and based on that the input is communicated to Expert System.
     These results are then returned.
     """
-
+    __expert_system = None
     def __init__(self) -> None:
-        pass
+        print("[CONSOLE] Initializing the Expert System...")
+        self.__expert_system = Allergy()
+        print("[CONSOLE] Expert System has been initialized successfuly!")
+
+    def run(self) -> None:
+        try:
+            symptoms = input("Enter symptoms you have: ")
+            symptoms = symptoms.lower()
+            symptoms = symptoms.replace(" ", '')
+            symptoms = symptoms.strip("\n")
+            symptoms =symptoms.split(" ")
+
+        except Exception as e:
+            print("[ERR] The following error occured while trying to run the user console: "+str(e))
